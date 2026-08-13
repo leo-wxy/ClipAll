3402fd457827e5dee5daaa04daa8012f70ecfb34
wangxiaoyong <wangxiaoyong@corp.netease.com>
2026-08-13 10:48:00 +0800

    chore(release): 发布 0.0.9

--- ClipAll/PluginHost/Manifest/ExternalPluginManifestMapper.swift
+++ ClipAll/PluginHost/Manifest/ExternalPluginManifestMapper.swift
@@ -32,7 +32,7 @@ struct SemanticVersion: Comparable, Equatable, Sendable {
 struct ExternalPluginManifestMapper: Sendable {
     let hostVersion: SemanticVersion
 
-    init(hostVersion: String = "0.0.8") {
+    init(hostVersion: String = "0.0.9") {
         guard let version = SemanticVersion(hostVersion) else {
             preconditionFailure("宿主版本必须是三段 SemVer")
         }
--- ClipAll/PluginHost/Validation/PluginPackageValidator.swift
+++ ClipAll/PluginHost/Validation/PluginPackageValidator.swift
@@ -22,7 +22,7 @@ struct PluginPackageValidator: Sendable {
 
     init(
         limits: PluginPackageLimits = .standard,
-        hostVersion: String = "0.0.8"
+        hostVersion: String = "0.0.9"
     ) {
         self.limits = limits
         manifestDecoder = ExternalPluginManifestDecoder()
